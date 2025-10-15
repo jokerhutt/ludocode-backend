e44edacf585157028eeaa13cfd5012892de8444f
Dave <davidglogowski2003@gmail.com>
2025-10-16 00:03:05 +0200

    Kotlin project setup

---
+++ src/main/kotlin/com/ludocode/ludocodebackend/LudocodeBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.ludocode.ludocodebackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LudocodeBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<LudocodeBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/ludocode/ludocodebackend/TestLudocodeBackendApplication.kt
@@ -0,0 +1,9 @@
+package com.ludocode.ludocodebackend
+
+import org.springframework.boot.fromApplication
+import org.springframework.boot.with
+
+
+fun main(args: Array<String>) {
+	fromApplication<LudocodeBackendApplication>().with(TestcontainersConfiguration::class).run(*args)
+}
