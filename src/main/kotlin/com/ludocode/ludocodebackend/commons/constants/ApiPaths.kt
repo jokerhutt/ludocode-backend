69e55d90c6b6bb52a7241d3278d9822fbf239d20
Dave <davidglogowski2003@gmail.com>
2026-01-05 16:36:05 +0100

    refactor(endpoints): Add ApiPaths object with base paths

---
+++ src/main/kotlin/com/ludocode/ludocodebackend/commons/constants/ApiPaths.kt
@@ -0,0 +1,40 @@
+package com.ludocode.ludocodebackend.commons.constants
+
+object ApiPaths {
+
+    const val API_PREFIX: String = "/api/v1"
+
+    object AI {
+        const val BASE : String = "/ai"
+    }
+
+    object CREDITS {
+        const val BASE: String = "/credits"
+    }
+
+    object AUTH {
+        const val BASE: String = "/auth"
+    }
+
+    object CATALOG {
+        const val BASE : String = "/catalog"
+    }
+
+    object PROJECT {
+        const val BASE : String = "/projects"
+    }
+
+    object PROGRESS {
+        const val BASE : String = "/progress"
+    }
+
+    object RUNNER {
+        const val BASE : String = "/runner"
+    }
+
+    object SNAPSHOT {
+        const val BASE : String = "/snapshot"
+    }
+
+
+}
