2abdf365d36df0160ff2eae3a494b9ff87e2792d
Dave <davidglogowski2003@gmail.com>
2026-01-08 20:10:46 +0100

    feat(api): Add OpenAPI plugin & docs

--- src/main/kotlin/com/ludocode/ludocodebackend/commons/constants/PublicEndpointConstants.kt
+++ src/main/kotlin/com/ludocode/ludocodebackend/commons/constants/PublicEndpointConstants.kt
@@ -4,13 +4,13 @@ object PublicEndpointConstants {
     @JvmField
     val PUBLIC_ENDPOINTS = arrayOf(
         "/actuator/**",
-        ApiPaths.AUTH.DEMO,
-        ApiPaths.AUTH.FIREBASE,
+        "${ApiPaths.AUTH.BASE}${ApiPaths.AUTH.DEMO}",
+        "${ApiPaths.AUTH.BASE}${ApiPaths.AUTH.FIREBASE}",
         ApiPaths.FEATURES.BASE,
-        ApiPaths.CATALOG.MODULES,
-        ApiPaths.CATALOG.COURSES,
-        ApiPaths.CATALOG.LESSON_EXERCISES,
-        ApiPaths.CATALOG.COURSE_TREE,
+        "${ApiPaths.CATALOG.BASE}${ApiPaths.CATALOG.MODULES}",
+        "${ApiPaths.CATALOG.BASE}${ApiPaths.CATALOG.COURSES}",
+        "${ApiPaths.CATALOG.BASE}${ApiPaths.CATALOG.LESSON_EXERCISES}",
+        "${ApiPaths.CATALOG.BASE}${ApiPaths.CATALOG.COURSE_TREE}",
         "${ApiPaths.AI.BASE}/**",
         "${ApiPaths.SNAPSHOTS}/**"
 
