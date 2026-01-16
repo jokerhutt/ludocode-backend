2571742e981f70587a1a5b683403ebf1ecf4d60d
Dave <davidglogowski2003@gmail.com>
2026-01-16 16:49:15 +0100

    feat(logging): Add logging packages and logback spring xml

---
+++ src/main/kotlin/com/ludocode/ludocodebackend/commons/constants/LogFields.kt
@@ -0,0 +1,13 @@
+package com.ludocode.ludocodebackend.commons.constants
+
+object LogFields {
+    const val REQUEST_ID = "requestId"
+    const val USER_ID = "userId"
+    const val PROJECT_ID = "projectId"
+
+    const val SIZE = "size"
+    const val HITS = "hits"
+    const val ADD_COUNT = "addCount"
+    const val UPDATE_COUNT = "updateCount"
+    const val DELETE_COUNT = "deleteCount"
+}
