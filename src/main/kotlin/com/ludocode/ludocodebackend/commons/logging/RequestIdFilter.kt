c36ef48c4661366d5e93f61da4758b7bb267f03e
Dave <davidglogowski2003@gmail.com>
2026-01-16 18:23:25 +0100

    feat(logging): Add request ID filter

---
+++ src/main/kotlin/com/ludocode/ludocodebackend/commons/logging/RequestIdFilter.kt
@@ -0,0 +1,31 @@
+package com.ludocode.ludocodebackend.commons.logging
+
+import com.ludocode.ludocodebackend.commons.constants.LogFields
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.slf4j.MDC
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+import java.util.UUID
+
+@Component
+class RequestIdFilter : OncePerRequestFilter() {
+
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        val requestId = request.getHeader("X-Request-Id") ?: UUID.randomUUID().toString()
+
+        MDC.put(LogFields.REQUEST_ID, requestId)
+        response.setHeader("X-Request-Id", requestId)
+
+        try {
+            filterChain.doFilter(request, response)
+        } finally {
+            MDC.remove(LogFields.REQUEST_ID)
+        }
+    }
+}
