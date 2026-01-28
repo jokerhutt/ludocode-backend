5b0789eb03d7fe37b9b053cb6ea5be3a6e22b255
Dave <davidglogowski2003@gmail.com>
2026-01-29 00:19:22 +0100

    feat(caching): Add Redis Configuration

--- src/main/kotlin/com/ludocode/ludocodebackend/LudocodeBackendApplication.kt
+++ src/main/kotlin/com/ludocode/ludocodebackend/LudocodeBackendApplication.kt
@@ -2,8 +2,10 @@ package com.ludocode.ludocodebackend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 
 @SpringBootApplication
+@EnableCaching
 class LudocodeBackendApplication
 
 fun main(args: Array<String>) {
