a5e24f504f39930ea95fb05a50529bd95b46adf8
Dave <davidglogowski2003@gmail.com>
2026-01-29 18:14:21 +0100

    disable redis password

---
+++ src/main/kotlin/com/ludocode/ludocodebackend/commons/configuration/RedisConfig.kt
@@ -0,0 +1,52 @@
+package com.ludocode.ludocodebackend.commons.configuration
+
+import io.lettuce.core.ClientOptions
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.serializer.StringRedisSerializer
+
+@Configuration
+class RedisConfig {
+
+    @Value("\${spring.data.redis.host}")
+    private lateinit var redisHost: String
+
+    @Value("\${spring.data.redis.port}")
+    private var redisPort: Int = 6379
+
+    @Value("\${spring.data.redis.password:}")
+    private var redisPassword: String = ""
+
+    @Bean
+    fun redisConnectionFactory(): RedisConnectionFactory {
+        val redisStandaloneConfiguration = RedisStandaloneConfiguration()
+        redisStandaloneConfiguration.hostName = redisHost
+        redisStandaloneConfiguration.port = redisPort
+
+        if (redisPassword.isNotBlank()) {
+            redisStandaloneConfiguration.setPassword(redisPassword)
+        }
+
+        val clientConfig = LettuceClientConfiguration.builder()
+            .clientOptions(ClientOptions.builder().build())
+            .build()
+
+        return LettuceConnectionFactory(redisStandaloneConfiguration, clientConfig)
+    }
+
+    @Bean
+    fun redisTemplate(connectionFactory: RedisConnectionFactory): RedisTemplate<String, Any> {
+        val template = RedisTemplate<String, Any>()
+        template.connectionFactory = connectionFactory
+        template.keySerializer = StringRedisSerializer()
+        template.valueSerializer = StringRedisSerializer()
+        return template
+    }
+}
+
