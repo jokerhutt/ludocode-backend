d4469434c00773fba3ece38dc6896b4aeb44c9bd
Dave <davidglogowski2003@gmail.com>
2026-02-21 00:23:29 +0100

    emit user created event after transaction

--- src/main/kotlin/com/ludocode/ludocodebackend/subscription/app/event/SubscriptionBootstrapListener.kt
+++ src/main/kotlin/com/ludocode/ludocodebackend/subscription/app/event/SubscriptionBootstrapListener.kt
@@ -1,17 +1,16 @@
 package com.ludocode.ludocodebackend.subscription.app.event
 import com.ludocode.ludocodebackend.subscription.app.service.SubscriptionService
 import com.ludocode.ludocodebackend.user.domain.event.UserRegisteredEvent
-import org.springframework.context.event.EventListener
-import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
+import org.springframework.transaction.event.TransactionPhase
+import org.springframework.transaction.event.TransactionalEventListener
 
 @Component
 class SubscriptionBootstrapListener(
     private val subscriptionService: SubscriptionService
 ) {
 
-    @Async
-    @EventListener
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     fun handle(event: UserRegisteredEvent) {
         subscriptionService.ensureSubscriptionExists(event.userId)
     }
