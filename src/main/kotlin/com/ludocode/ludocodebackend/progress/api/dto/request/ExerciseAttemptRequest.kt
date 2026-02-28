8ab68259ae6ff458e300a7124567fb4d6fe43ee9
Dave <davidglogowski2003@gmail.com>
2026-02-28 19:11:30 +0100

    refacotr(lesson submissions): Remove ExerciseAttemptRequest.kt wrapper (#172)

--- src/main/kotlin/com/ludocode/ludocodebackend/progress/api/dto/request/ExerciseAttemptRequest.kt
+++
@@ -1,7 +0,0 @@
-package com.ludocode.ludocodebackend.progress.api.dto.request
-
-import com.ludocode.ludocodebackend.lesson.domain.jsonb.ExerciseAnswer
-
-data class ExerciseAttemptRequest(
-    val answer: ExerciseAnswer
-)
