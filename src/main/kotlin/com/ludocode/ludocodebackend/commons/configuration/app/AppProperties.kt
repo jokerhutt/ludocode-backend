9b64788b0ed69e87d108a8d256cb2de41b99d60f
Dave <davidglogowski2003@gmail.com>
2026-03-02 16:56:48 +0100

    remove open to public guard

--- src/main/kotlin/com/ludocode/ludocodebackend/commons/configuration/app/AppProperties.kt
+++ src/main/kotlin/com/ludocode/ludocodebackend/commons/configuration/app/AppProperties.kt
@@ -5,5 +5,4 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 @ConfigurationProperties(prefix = "app")
 data class AppProperties(
     val frontendUrl: String,
-    val openToPublic: Boolean
 )
