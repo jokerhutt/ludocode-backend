b88c7e7ca34b61f6d57fd06f0d7dde81609e7347
Dave <davidglogowski2003@gmail.com>
2026-03-07 17:57:22 +0100

    add test for no deleting last course

--- src/test/kotlin/com/ludocode/ludocodebackend/catalog/integration/DeleteCourseIT.kt
+++ src/test/kotlin/com/ludocode/ludocodebackend/catalog/integration/DeleteCourseIT.kt
@@ -2,8 +2,10 @@ package com.ludocode.ludocodebackend.catalog.integration
 
 import com.ludocode.ludocodebackend.catalog.api.dto.response.CourseResponse
 import com.ludocode.ludocodebackend.commons.constants.ApiPaths
+import com.ludocode.ludocodebackend.commons.exception.ErrorCode
 import com.ludocode.ludocodebackend.support.AbstractIntegrationTest
 import com.ludocode.ludocodebackend.support.TestRestClient
+import io.restassured.response.ValidatableResponse
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import java.util.UUID
@@ -28,8 +30,22 @@ class DeleteCourseIT : AbstractIntegrationTest() {
         assertThat(res.size).isEqualTo(courses.size - 1)
         assertThat(res)
             .noneMatch { it.id == courseToDelete.id }
+    }
 
+    @Test
+    fun deleteCourse_deletesCourse_thenTriesToDeleteLast_throwsError() {
 
+        val courses = submitGetAllCourses()
+        assertThat(courses.size).isEqualTo(2)
+
+        submitDeleteCourse(courses[1].id)
+
+        courseRepository.flush()
+        moduleRepository.flush()
+        moduleLessonsRepository.flush()
+
+        val courseToDeleteId = courses[0].id
+        assertErrorOnDelete(courseToDeleteId, ErrorCode.NO_LAST_COURSE_DELETE)
 
 
 
@@ -43,6 +59,10 @@ class DeleteCourseIT : AbstractIntegrationTest() {
                 Array<CourseResponse>::class.java
             )
             .toList()
+
+    private fun assertErrorOnDelete(courseId: UUID, errorCode: ErrorCode): ValidatableResponse? {
+        return TestRestClient.assertError("DELETE", ApiPaths.SNAPSHOTS.byCourseAdmin(courseId), user1.id!!, null, errorCode)
+    }
 
     private fun submitGetAllCourses(): List<CourseResponse> =
         TestRestClient
