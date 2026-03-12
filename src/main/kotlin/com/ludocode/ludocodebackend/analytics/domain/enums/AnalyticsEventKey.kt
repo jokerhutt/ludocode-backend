abe741415237690cb141827f0fbc379b3e71f533
Dave <davidglogowski2003@gmail.com>
2026-03-12 13:45:50 +0100

    add navigation click event

--- src/main/kotlin/com/ludocode/ludocodebackend/analytics/domain/enums/AnalyticsEventKey.kt
+++ src/main/kotlin/com/ludocode/ludocodebackend/analytics/domain/enums/AnalyticsEventKey.kt
@@ -3,6 +3,7 @@ package com.ludocode.ludocodebackend.analytics.domain.enums
 enum class AnalyticsEventKey {
     SIGNUP_CLICK,
     LOGIN_CLICK,
+    NAVIGATION_CLICK,
     DOCS_CLICK,
     SOURCE_CODE_CLICK,
     LANDING_PAGE_VIEW
