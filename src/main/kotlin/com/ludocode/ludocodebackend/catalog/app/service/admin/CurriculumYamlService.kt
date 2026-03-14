b0aa96e89b6a515490c05d1511b662e3b4c42f84
Dave <davidglogowski2003@gmail.com>
2026-03-14 21:44:09 +0100

    disable use native type id yaml mapping

--- src/main/kotlin/com/ludocode/ludocodebackend/catalog/app/service/admin/CurriculumYamlService.kt
+++ src/main/kotlin/com/ludocode/ludocodebackend/catalog/app/service/admin/CurriculumYamlService.kt
@@ -3,6 +3,7 @@ package com.ludocode.ludocodebackend.catalog.app.service.admin
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.ludocode.ludocodebackend.catalog.api.dto.request.CreateCourseRequest
@@ -31,7 +32,7 @@ class CurriculumYamlService(
 
 
     private val yamlMapper =
-        ObjectMapper(YAMLFactory())
+        ObjectMapper(YAMLFactory().disable(YAMLGenerator.Feature.USE_NATIVE_TYPE_ID))
             .registerModule(KotlinModule.Builder().build())
             .registerModule(JavaTimeModule())
             .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
